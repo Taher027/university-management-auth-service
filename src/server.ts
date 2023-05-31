8d0f3557d4144a021f7152a70e62742adf448ecd
Taher027 <abutaherh27@gmail.com>
2023-05-31 10:40:05 +0600

    husky test

--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,15 @@
-import mongoose from "mongoose";
-import app from "./app";
-import config from "./config/index";
+import mongoose from 'mongoose'
+import app from './app'
+import config from './config/index'
 
 async function dbConnected() {
   try {
-    await mongoose.connect(config.database_url as string);
+    await mongoose.connect(config.database_url as string)
     app.listen(config.port, () => {
-      console.log(`Application listening on port ${config.port}`);
-    });
+      console.log(`Application listening on port ${config.port}`)
+    })
   } catch (error) {
-    console.log("failed to connect", error);
+    console.log('failed to connect', error)
   }
 }
-dbConnected();
+dbConnected()
